385813e32527374e66abca398bfbdd5abf75a489
maneetk111 <98216565+maneetk111@users.noreply.github.com>
2022-01-23 23:37:29 +0530

    Add files via upload

---
+++ Java/Activities/Activity12.java
@@ -0,0 +1,18 @@
+package activities;
+
+interface Addable{
+	int add(int num1, int num2);
+}
+
+public class Activity12 {
+
+	public static void main(String[] args) {
+		Addable ad1 = (a, b) -> (a + b);
+		Addable ad2 = (int a, int b) -> {
+			return (a+b);
+		};
+		System.out.println(ad1.add(10, 20));
+		System.out.println(ad2.add(50, 60));
+		
+	}
+}
